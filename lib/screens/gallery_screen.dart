57e098606df515512b6b588ae88ff0697dc5427d
Isaac Thiessen <fargusplumdoodle@gmail.com>
2025-05-25 18:00:28 -0700

    separated gallery image widget

--- lib/screens/gallery_screen.dart
+++ lib/screens/gallery_screen.dart
@@ -1,33 +1,10 @@
-import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import '../controllers/photo_journal_controller.dart';
+import '../widgets/gallery_image.dart';
 
 class GalleryScreen extends StatelessWidget {
   const GalleryScreen({super.key});
-
-  String _formatDateForDisplay(String date) {
-    try {
-      final dateTime = DateTime.parse(date);
-      const months = [
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec',
-      ];
-      return '${months[dateTime.month - 1]} ${dateTime.day}, ${dateTime.year}';
-    } catch (e) {
-      return date;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,78 +63,7 @@ class GalleryScreen extends StatelessWidget {
             itemCount: entries.length,
             itemBuilder: (context, index) {
               final entry = entries[index];
-              final photoPath = entry.stitchedPhotoPath!;
-
-              return Container(
-                margin: const EdgeInsets.only(bottom: 24),
-                decoration: BoxDecoration(
-                  color: CupertinoColors.systemBackground,
-                  borderRadius: BorderRadius.circular(16),
-                  boxShadow: [
-                    BoxShadow(
-                      color: CupertinoColors.systemGrey.withOpacity(0.1),
-                      blurRadius: 10,
-                      offset: const Offset(0, 2),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Date header
-                    Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Text(
-                        _formatDateForDisplay(entry.date),
-                        style: CupertinoTheme.of(context).textTheme.textStyle
-                            .copyWith(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                      ),
-                    ),
-
-                    // Photo
-                    ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(16),
-                        bottomRight: Radius.circular(16),
-                      ),
-                      child: AspectRatio(
-                        aspectRatio: 16 / 9,
-                        child: File(photoPath).existsSync()
-                            ? Image.file(
-                                File(photoPath),
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return Container(
-                                    color: CupertinoColors.systemGrey6,
-                                    child: const Center(
-                                      child: Icon(
-                                        CupertinoIcons.exclamationmark_triangle,
-                                        size: 40,
-                                        color: CupertinoColors.systemGrey3,
-                                      ),
-                                    ),
-                                  );
-                                },
-                              )
-                            : Container(
-                                color: CupertinoColors.systemGrey6,
-                                child: const Center(
-                                  child: Icon(
-                                    CupertinoIcons.photo,
-                                    size: 40,
-                                    color: CupertinoColors.systemGrey3,
-                                  ),
-                                ),
-                              ),
-                      ),
-                    ),
-                  ],
-                ),
-              );
+              return GalleryImage(entry: entry);
             },
           );
         }),
---
+++ lib/widgets/gallery_image.dart
@@ -0,0 +1,140 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import '../models/daily_entry.dart';
+import '../utils/location_formatter.dart';
+
+class GalleryImage extends StatelessWidget {
+  final DailyEntry entry;
+
+  const GalleryImage({super.key, required this.entry});
+
+  String _formatDateForDisplay(String date) {
+    try {
+      final dateTime = DateTime.parse(date);
+      const months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+      ];
+      return '${months[dateTime.month - 1]} ${dateTime.day}, ${dateTime.year}';
+    } catch (e) {
+      return date;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Get.snackbar(
+          'TODO: ONTAP',
+          'Gallery image tap functionality not implemented yet',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: CupertinoColors.systemBlue,
+          colorText: CupertinoColors.white,
+          duration: const Duration(seconds: 2),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 24),
+        decoration: BoxDecoration(
+          color: CupertinoColors.systemBackground,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: CupertinoColors.systemGrey.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Date and location header
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    _formatDateForDisplay(entry.date),
+                    style: CupertinoTheme.of(context).textTheme.textStyle
+                        .copyWith(fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                  FutureBuilder<String>(
+                    future: getFormattedLocation(
+                      entry.latitude,
+                      entry.longitude,
+                    ),
+                    builder: (context, snapshot) {
+                      return Text(
+                        snapshot.data ?? 'Loading...',
+                        style: CupertinoTheme.of(context).textTheme.textStyle
+                            .copyWith(
+                              fontSize: 14,
+                              color: CupertinoColors.systemGrey,
+                            ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            // Photo
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child:
+                    entry.stitchedPhotoPath != null &&
+                        File(entry.stitchedPhotoPath!).existsSync()
+                    ? Image.file(
+                        File(entry.stitchedPhotoPath!),
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            color: CupertinoColors.systemGrey6,
+                            child: const Center(
+                              child: Icon(
+                                CupertinoIcons.exclamationmark_triangle,
+                                size: 40,
+                                color: CupertinoColors.systemGrey3,
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                    : Container(
+                        color: CupertinoColors.systemGrey6,
+                        child: const Center(
+                          child: Icon(
+                            CupertinoIcons.photo,
+                            size: 40,
+                            color: CupertinoColors.systemGrey3,
+                          ),
+                        ),
+                      ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
